cfd618a1dd8f887146e6720420b56519e278542a
JackLee <1071426378@163.com>
2024-12-29 23:10:52 +0800

    add scale and move function

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -28,6 +28,7 @@ main {
     img {
       transform-origin: top left;
       position: relative;
+      cursor: pointer;
     }
   }
 
@@ -97,14 +98,25 @@ main {
   footer {
     height: 20px;
     background: #444;
+    font-size: 14px;
+    color: #fff;
   }
 }
 </style>
 
 <template>
 <main>
-  <div id="content" ref='content'>
-    <img @load="doneImg" ref='imgView' :src="`file:${img}`" alt="loading..." :style="`transform: scale(${scale});left: ${left}px;top: ${top}px;`">
+  <div id="content" ref='content' @wheel="scaleListener">
+    <img @load="doneImg"
+         @mousedown="drag_down"
+         @mousemove="drag"
+         @mouseup="drag_up"
+         @mouseleave="drag_up"
+         draggable="false"
+         ref='imgView'
+         :src="`file:${img}`"
+         alt="loading..."
+         :style="`transform: scale(${scale});left: ${left}px;top: ${top}px;`">
   </div>
   <div id="align-line">
     <div class="align-indicator align-2"></div>
@@ -120,7 +132,7 @@ main {
     </div>
   </div>
   <footer>
-
+    {{scale * 100}}
   </footer>
 </main>
 </template>
@@ -178,6 +190,8 @@ const left = ref(0)
 const top = ref(0)
 const offsetLeft = ref(0)
 const offsetTop = ref(0)
+const anchorLeft = ref(50)
+const anchorTop = ref(50)
 
 onMounted(() => {
   console.log(content.value)
@@ -186,7 +200,7 @@ onMounted(() => {
     for (let i of e) {
       const contentWidth = i.contentRect.width
       const contentHeight = i.contentRect.height
-
+      calcCorrectLeftTop()
       // resizeDefaultRatio(contentWidth, contentHeight)
 
     }
@@ -228,23 +242,85 @@ const initScale = () => {
   top.value = contentHeight / 2 - newHeight / 2
 }
 
-const resizeDefaultScale = (contentWidth,contentHeight) => {
-
-  console.log(contentWidth, contentHeight)
-}
-
-const calcCorrectLeftTop = () => {
+const scaleListener = (e) => {
+  if (!img.value) return
+  // console.log(e.deltaY)
   const width = imgView.value.getBoundingClientRect().width
   const height = imgView.value.getBoundingClientRect().height
+  const s = scale.value
+  scale.value -= e.deltaY * scale.value * 0.0005
+
+  offsetLeft.value = offsetLeft.value * scale.value / s
+  offsetTop.value = offsetTop.value * scale.value / s
+
+  if (scale.value < 0) scale.value = 0
+  calcCorrectLeftTop(width * scale.value / s, height * scale.value / s)
+}
+
+const calcCorrectLeftTop = (width: number | null = null, height: number | null = null) => {
+  width = width != null ? width : imgView.value.getBoundingClientRect().width
+  height = height != null ? height : imgView.value.getBoundingClientRect().height
   const contentWidth = content.value.getBoundingClientRect().width
   const contentHeight = content.value.getBoundingClientRect().height
-  let mLeft = contentWidth / 2 - width / 2
-  let mTop = contentHeight / 2 - height / 2
-  if (width > contentWidth) mLeft = left.value + offsetLeft.value
-  if (height > contentHeight) mTop = top.value + offsetTop.value
+  let mLeft = contentWidth / 2 - width! / 2
+  let mTop = contentHeight / 2 - height! / 2
+  if (width! > contentWidth) {
+    mLeft = mLeft + offsetLeft.value
+    if (mLeft > 0) {
+      mLeft = 0
+      offsetLeft.value = width! / 2 - contentWidth / 2
+    }
+    if (mLeft < contentWidth - width!) {
+      mLeft = contentWidth - width!
+      offsetLeft.value = contentWidth / 2 - width! / 2
+    }
+  } else {
+    offsetLeft.value = 0
+  }
+  if (height! > contentHeight) {
+    mTop = mTop + offsetTop.value
+    if (mTop > 0) {
+      mTop = 0
+      offsetTop.value = height! / 2 - contentHeight / 2
+    }
+    if (mTop < contentHeight - height!) {
+      mTop = contentHeight - height!
+      offsetTop.value = contentHeight / 2 - height! / 2
+    }
+  } else {
+    offsetTop.value = 0
+  }
+
   left.value = mLeft
   top.value = mTop
 }
+
+const dragStartX = ref(null as number | null)
+const dragStartY = ref(null as number | null)
+const dragCurrentX = ref(null as number | null)
+const dragCurrentY = ref(null as number | null)
+
+const drag_down = (e) => {
+  dragStartX.value = e.clientX
+  dragStartY.value = e.clientY
+  dragCurrentX.value = offsetLeft.value
+  dragCurrentY.value = offsetTop.value
+}
+
+const drag = (e) => {
+  e.preventDefault()
+  if (dragStartX.value === null || dragStartY.value === null) return
+  offsetLeft.value = dragCurrentX.value + e.clientX - dragStartX.value
+  offsetTop.value = dragCurrentY.value + e.clientY - dragStartY.value
+  calcCorrectLeftTop()
+}
+
+const drag_up = () => {
+  dragStartX.value = null
+  dragStartY.value = null
+}
+
+
 
 
 </script>
